59dfb7c51f0e59734c02e4cac98475869b65308e
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-11-17 12:08:06 -0600

    added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({cmd: 'greeting'})
+  getGreetingmessage(name: string): string {
+    return `Hello ${name}`;
+  }
+
+
+  @MessagePattern({cmd: 'greeting-async'})
+  async getGreetingMessageAsync(name: string): Promise<string> {
+    return `Hello ${name}`;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {name: 'GREETING_SERVICE', transport: Transport.TCP,
+        options: {
+        host: 'localhost',
+        port: 8080
+      }
+      }])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
